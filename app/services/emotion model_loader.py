d9ccdde44116a4c308c9f6dacbe319cdbeb6f122
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-04-25 02:22:59 +0900

    Update emotion model_loader.py

--- app/services/emotion model_loader.py
+++ app/services/emotion model_loader.py
@@ -1,1 +1,10 @@
+import os
+from transformers import TFBertForSequenceClassification, BertTokenizer
 
+MODEL_BASE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'models', 'intent')
+
+def load_model(version: str):
+    model_path = os.path.join(MODEL_BASE_PATH, version)
+    model = TFBertForSequenceClassification.from_pretrained(model_path)
+    tokenizer = BertTokenizer.from_pretrained(model_path)
+    return model, tokenizer
