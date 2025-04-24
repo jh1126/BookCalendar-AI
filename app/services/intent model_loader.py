db99d79c47f10b4d177a944f8a6bb516c4706005
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-04-25 02:59:39 +0900

    Update intent model_loader.py

--- app/services/intent model_loader.py
+++ app/services/intent model_loader.py
@@ -3,6 +3,7 @@ from transformers import TFBertForSequenceClassification, BertTokenizer
 
 MODEL_BASE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'models', 'intent')
 
+# 모델 로딩/버전 관리 함수
 def load_intent_model(version: str): # 예를 들어 version = intent_model_v1
     model_path = os.path.join(MODEL_BASE_PATH, version) 
     model = TFBertForSequenceClassification.from_pretrained(model_path) 
