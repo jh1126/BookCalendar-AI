0add3b1863a3f0b01bb92cc97bb4d66a0dc71707
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-04-26 15:06:19 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,23 +9,24 @@ app = FastAPI(title="AI API", version="1.0")
 
 # 감정 분석 관련 라우터 등록
 app.include_router(emotion_train.router, prefix="/emotion", tags=["Emotion"]) # 학습 & 검증
-app.include_router(emotion_model_version.router, prefix="/emotion", tags=["Emotion"]) # 모델 배포 기능
-# 모델 버전 확인
+app.include_router(emotion_version.router, prefix="/emotion", tags=["Emotion"]) # 모델 배포 기능
+app.include_router(emotion_version_confirm.router, prefix="/emotion", tags=["Emotion"])# 모델 버전 확인
 app.include_router(emotion_settings.router, prefix="/emotion", tags=["Emotion"]) # 모델 파라메터 수정
-app.include_router(emotion_settings.router, prefix="/emotion", tags=["Emotion"]) # 모델 삭제 
-# 장애 기록 정보 제공
+app.include_router(emotion_delete.router, prefix="/emotion", tags=["Emotion"]) # 모델 삭제 
+# 장애 기록 정보 제공(미완료)
 app.include_router(emotion_predict.router, prefix="/emotion", tags=["emotion"]) # 모델 예측
 
 # 의도 분류 관련 라우터 등록
-app.include_router(emotion_train.router, prefix="/emotion", tags=["intent"]) # 학습 & 검증
-app.include_router(emotion_model_version.router, prefix="/emotion", tags=["intent"]) # 모델 배포 기능
+app.include_router(intent_train.router, prefix="/intent", tags=["intent"]) # 학습 & 검증
+app.include_router(intent_model_version.router, prefix="/intent", tags=["intent"]) # 모델 배포 기능
 # 모델 버전 확인
-app.include_router(emotion_settings.router, prefix="/emotion", tags=["intent"]) # 모델 파라메터 수정
-app.include_router(emotion_settings.router, prefix="/emotion", tags=["intent"]) # 모델 삭제 
-# 장애 기록 정보 제공
-app.include_router(emotion_predict.router, prefix="/emotion", tags=["intent"]) # 모델 예측
+app.include_router(intent_settings.router, prefix="/intent", tags=["intent"]) # 모델 파라메터 수정
+app.include_router(intent_settings.router, prefix="/intent", tags=["intent"]) # 모델 삭제 
+# 장애 기록 정보 제공(미완료)
+app.include_router(intent_predict.router, prefix="/intent", tags=["intent"]) # 모델 예측
 
-# 헬스 체크용 루트 엔드포인트
+
+
 @app.get("/")
 def read_root():
     return {"message": "AI 감정 분석 서버 작동 중!"}
