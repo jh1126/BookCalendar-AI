3f387aac8bcdf84dbe6cdbe6fbd6f55b0fa81576
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-04-26 15:41:43 +0900

    Update emotion model_loader.py

--- app/services/emotion/emotion model_loader.py
+++ app/services/emotion/emotion model_loader.py
@@ -1,7 +1,7 @@
 import os
 from transformers import TFBertForSequenceClassification, BertTokenizer
 
-MODEL_BASE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', 'models', 'emotion')
+MODEL_BASE_PATH = os.path.join(os.path.dirname(__file__), '..', '..','..', 'models', 'emotion')
 
 # 모델 로딩/버전 관리 함수
 def load_emotion_model(version: str): # 예를 들어 version = emotion_model_v1
