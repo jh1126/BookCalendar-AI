37f338c0418da8df11499774e5e4f3610a180f3f
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-02 01:44:48 +0900

    Update model_require.py

--- app/routes/model_require.py
+++ app/routes/model_require.py
@@ -1,9 +1,22 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+
 router = APIRouter()
 
+# 모델 기록 불러오기
+def load_metrics():
+    if os.path.exists(METRICS_FILE):
+        with open(METRICS_FILE, "r") as f:
+            return json.load(f)
+    return []
+    
+def load_inform():
+    emotion_file = os.path.join(os.path.dirname(__file__), '..','..','..','data','emotion','emotion_model_metrics.json')
+    intent_file = os.path.join(os.path.dirname(__file__), '..','..','..','data','intent','intent_model_metrics.json')
 
+    
+    
 
 # FastAPI 엔드포인트 정의
 @router.get("/modelRequire")
