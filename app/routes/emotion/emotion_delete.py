263d897ff75c47c5064fa8e09ac841db4a2c685e
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-01 10:10:51 +0900

    Update emotion_delete.py

--- app/routes/emotion/emotion_delete.py
+++ app/routes/emotion/emotion_delete.py
@@ -4,19 +4,23 @@ from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
 
-@router.delete("/delete_emtion/{version}")
-def delete_emotion_model(version: str):
+class TextInput(BaseModel):
+    deleteModelName: str
+
+
+@router.post("/deleteModel")
+def delete_emotion_model(data: TextInput):
     """지정한 버전의 감정 모델 디렉토리 삭제"""
-    
+
+    model_name = data.deleteModelName
     model_dir = os.path.join(
-        os.path.dirname(__file__), '..', '..','..', 'models', 'emotion', f"emotion_model_{version}"
+        os.path.dirname(__file__), '..', '..','..', 'models', 'emotion', f"emotion_model_{model_name}"
     )
 
     if os.path.exists(model_dir):
         shutil.rmtree(model_dir)  # 디렉터리 삭제
         return {
-            "message": f"{version} 버전 감정 모델이 삭제되었습니다.",
-            "deleted_path": model_dir
+            "message": f"{model_name) 버전 감정 모델이 삭제되었습니다."
         }
     else:
         raise HTTPException(status_code=404, detail=f"{version} 버전 모델이 존재하지 않습니다.")
