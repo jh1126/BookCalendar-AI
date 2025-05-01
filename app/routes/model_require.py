dd52d6d8828324fe8396f92c12a3cf18cdf4ddc6
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-01 20:08:28 +0900

    Update model_require.py

--- app/routes/model_require.py
+++ app/routes/model_require.py
@@ -7,4 +7,4 @@ from pydantic import BaseModel
 # FastAPI 엔드포인트 정의
 @router.get("/modelRequire")
 def model_inform():
-    return {"message": "AI 감정 분석 서버 작동 중!"}
+    return {"message": "AI 서버 작동 중"}
