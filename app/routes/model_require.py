b0919f21e3c7cf259f0a1a0f5778b4e8d32085be
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-01 20:24:20 +0900

    Update model_require.py

--- app/routes/model_require.py
+++ app/routes/model_require.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 router = APIRouter()
 
@@ -8,4 +8,4 @@ router = APIRouter()
 # FastAPI 엔드포인트 정의
 @router.get("/modelRequire")
 def model_inform():
-    return {"message": "AI 서버 작동 중"}
+    return JSONResponse(content={"message": "AI 서버 작동 중"})
