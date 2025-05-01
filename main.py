a27f5a5251483938b1b0a47643294a483e4b3611
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-01 18:25:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,12 @@ from app.routes.intent import (
 
 app = FastAPI(title="AI API", version="1.0")
 
+#/modelRequire ({모델 이름, 파라메터, 성능지표}json 리턴, 서비스에 사용중인 모델, 자동학습여부)
+app.include_router(model_require.router, prefix="", tags=["all"])
+
 # 감정 분석 관련 라우터 등록(관리자 서버)
 app.include_router(emotion_train.router, prefix="emotion", tags=["Emotion"]) # 학습 & 검증
 app.include_router(emotion_version.router, prefix="emotion", tags=["Emotion"]) # 모델 배포 기능
-#app.include_router(emotion_version_confirm.router, prefix="emotion", tags=["Emotion"])# 모델 버전 확인
 app.include_router(emotion_delete.router, prefix="emotion", tags=["Emotion"]) # 모델 삭제 
 #app.include_router(emotion_logs.router, prefix="emotion", tags=["Emotion"])# 장애 기록 정보 제공(미완료)
 # 감정 분석 예측 (서비스 서버)
@@ -34,7 +36,6 @@ app.include_router(emotion_predict.router, prefix="emotion", tags=["emotion"]) #
 # 의도 분류 관련 라우터 등록(관리자 서버)
 app.include_router(intent_train.router, prefix="intent", tags=["intent"]) # 학습 & 검증
 app.include_router(intent_version.router, prefix="intent", tags=["intent"]) # 모델 배포 기능
-#app.include_router(intent_version_confirm.router, prefix="intent", tags=["intent"])# 모델 버전 확인
 app.include_router(intent_delete.router, prefix="intent", tags=["intent"]) # 모델 삭제 
 #app.include_router(intent_logs.router, prefix="intent", tags=["intent"]) # 장애 기록 정보 제공(미완료)
 # 의도 분류 예측 (서비스 서버)
