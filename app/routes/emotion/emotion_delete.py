f82752d8d4dbbc895e2e1e8f646ab743ed6ae672
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-01 20:51:58 +0900

    Update emotion_delete.py

--- app/routes/emotion/emotion_delete.py
+++ app/routes/emotion/emotion_delete.py
@@ -12,13 +12,5 @@ class TextInput(BaseModel):
 @router.post("/delete_emotion")
 def delete_emotion_model(data: TextInput):
     """지정한 버전의 감정 모델 디렉토리 삭제"""
+    return JSONResponse(content={"questionModel": "abcd"})
 
-    model_name = data.deleteModelName
-    model_dir = os.path.join(
-        os.path.dirname(__file__), '..', '..','..', 'models', 'emotion', model_name)
-
-    if os.path.exists(model_dir):
-        shutil.rmtree(model_dir)  # 디렉터리 삭제
-        
-    else:
-        raise HTTPException(status_code=404, detail=f"{model_name} 버전 모델이 존재하지 않습니다.")
