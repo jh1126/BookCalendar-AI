9475e009950e2c46bddcdfaa87c916f5f75c80a2
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-16 03:45:48 +0900

    Update table.py

--- table.py
+++ table.py
@@ -1,1 +1,12 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text
 
+Base = declarative_base()
+
+class EmotionLog(Base):
+    __tablename__ = "emotionData"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(String(7), nullable=False)
+    input = Column(Text, nullable=False)
+    output = Column(String(20), nullable=False)
