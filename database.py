58022aa9572f21928ea81140f956e2bf6794c115
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-16 04:36:30 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,22 @@
 import pymysql
+import os
 
+def load_password(path):
+    with open(path, "r") as file:
+        token = file.read().strip()
+    return password
+    
 def get_connection():
+    
+    pass_path = os.path.join(os.path.dirname(__file__), 'secret', 'db_pass.txt')
+    pass_path = os.path.abspath(pass_path)
+    
     return pymysql.connect(
         host='localhost',
-        user='root',
-        password='비밀번호',
+        user='bookcalendar123',
+        password=load_password(pass_path),
         db='bookcalendar',
+        port=3307,
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
