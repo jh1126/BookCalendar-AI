e2616e1b30bd89e1ba9d7a9a1fc32629cea243e6
jh1126 <151304732+jh1126@users.noreply.github.com>
2025-05-16 03:46:09 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,11 @@
+import pymysql
 
+def get_connection():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='비밀번호',
+        db='bookcalendar',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
