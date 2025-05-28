e06ca9f36e683d1d4cba773ea60e4d02e5d31287
shqtbz143 <109617606+shqtbz143@users.noreply.github.com>
2025-05-28 16:37:03 +0900

    Update question_predict.py

--- app/routes/question/question_predict.py
+++ app/routes/question/question_predict.py
@@ -45,8 +45,8 @@ def load_model_and_tokenizer():
     if not os.path.exists(os.path.join(model_path, "config.json")):
         raise HTTPException(status_code=500, detail=f"모델 디렉토리 {model_path}에 HuggingFace 모델이 없습니다.")
 
-    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_path, trust_remote_code=True)
+    tokenizer = PreTrainedTokenizerFast(tokenizer_file=os.path.join(model_path, "tokenizer.json"))
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
 
 
     return tokenizer, model
